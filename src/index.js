f1f4112a435139485984854d5c55d60c53c8aab8
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-06-07 18:42:00 +0200

    javascript update, tabbed area now work correcetly

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
+const tabsButtons = document.querySelectorAll(".tabs-button");
+const tabsContentItems = document.querySelectorAll(".tabs-content-item");
 
+const selectItem = function () {
+  //removing show class from tab content
+  removeShow();
+
+  //selecting individual tab content based on ID
+  const tabsContentItem = document.querySelector(`#${this.id}-content`);
+  console.log(tabsContentItem);
+
+  //adds show class to content
+  tabsContentItem.classList.add("show");
+};
+
+tabsButtons.forEach((item) => {
+  item.addEventListener("click", selectItem);
+});
+
+//Removing the show class from content
+function removeShow() {
+  tabsContentItems.forEach((item) => {
+    item.classList.remove("show");
+  });
+}
